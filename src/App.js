027cf7fb27a34e1d3654ff678feae70d15447e9b
Anh Duong <anh.duong@apps-cyclone.com>
2022-10-12 15:52:42 +0700

    edit network

--- src/App.js
+++ src/App.js
@@ -5,30 +5,18 @@ import "react-toastify/dist/ReactToastify.css";
 const Web3 = require("web3");
 
 const networks = {
-  polygon: {
-    chainId: `0x${Number(137).toString(16)}`,
-    chainName: "Polygon Mainnet",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18
-    },
-    rpcUrls: ["https://polygon-rpc.com/"],
-    blockExplorerUrls: ["https://polygonscan.com/"]
-  },
   bsc: {
-    chainId: `0x${Number(56).toString(16)}`,
-    chainName: "Binance Smart Chain Mainnet",
+    chainId: `0x${Number(97).toString(16)}`,
+    chainName: "Binance Smart Chain Testnet",
     nativeCurrency: {
       name: "Binance Chain Native Token",
       symbol: "BNB",
       decimals: 18
     },
     rpcUrls: [
-      "https://bsc-dataseed1.binance.org",
-      "https://bsc-dataseed2.binance.org",
-      "https://bsc-dataseed3.binance.org",
-      "https://bsc-dataseed4.binance.org",
+      "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      "https://data-seed-prebsc-1-s2.binance.org:8545/",
+      "https://data-seed-prebsc-1-s3.binance.org:8545/",
       "https://bsc-dataseed1.defibit.io",
       "https://bsc-dataseed2.defibit.io",
       "https://bsc-dataseed3.defibit.io",
@@ -39,7 +27,7 @@ const networks = {
       "https://bsc-dataseed4.ninicoin.io",
       "wss://bsc-ws-node.nariox.org"
     ],
-    blockExplorerUrls: ["https://bscscan.com"]
+    blockExplorerUrls: ["https://testnet.bscscan.com"]
   }
 };
 
